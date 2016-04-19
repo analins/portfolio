71bdcaa8aa89ab3309c7651b6e143dcd60a947e5
ana lins <a.navarrolins@gmail.com>
2016-04-19 14:46:12 -0400

    changes homepage

--- scripts.js
+++ scripts.js
@@ -33,6 +33,7 @@ function renderProjects() {
     $('.contact-section').hide();
     $('.project-display').show('slow');
   });
+  viewProject();
 }
 
 function renderContact() {
@@ -41,3 +42,29 @@ function renderContact() {
     $('.contact-section').show('slow');
   });
 }
+
+
+
+// PROJECTZ
+
+function viewProject() {
+
+  $('.project').each(function(index){
+      $(this).attr('id', 'proj' + (index));
+    });
+
+
+$('.project').click(function(){
+if ($(this).hasClass('opened')){
+      $(this).removeClass('opened');
+      $('.project').fadeIn('fast');
+  }
+else{
+	  $(".project").index(this);
+    $(this).addClass('opened');
+    $('.opened').find('.links').fadeIn('fast');
+    $(".project").not(this).removeClass('opened');
+  }
+});
+
+};
